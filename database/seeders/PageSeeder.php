895cd9ae11164b49e173630192a661f4f1457fdc
Matt Daneshvar <matt.daneshvar@gmail.com>
2022-11-27 21:25:43 -0600

    Add parent page to seeder

--- database/seeders/PageSeeder.php
+++ database/seeders/PageSeeder.php
@@ -9,11 +9,19 @@ class PageSeeder extends Seeder
 {
     public function run()
     {
+        $faq = Page::factory()->create([
+            'title' => 'FAQ',
+            'slug' => 'faq',
+        ]);
+
         Page::factory()->create([
             'title' => 'Hello World',
             'slug' => 'hello-world',
+            'parent_id' => $faq->uuid
         ]);
 
-        Page::factory(9)->create();
+        Page::factory(9)->create([
+            'parent_id' => $faq->uuid
+        ]);
     }
 }
